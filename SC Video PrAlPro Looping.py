b5edc6f8528a05fa816233649343fb7aeb3fab8e
giannathannn <78425787+giannathannn@users.noreply.github.com>
2021-03-08 20:38:55 +0700

    Add files via upload

---
+++ SC Video PrAlPro Looping.py
@@ -0,0 +1,16 @@
+'''
+Codeforces 271 A 
+Beautiful Year
+'''
+
+y = int(input())
+
+while True:
+    y += 1
+    a = y // 1000
+    b = (y // 100) % 10
+    c = (y // 10) % 10
+    d = y % 10
+    if (a != b and a != c and a != d and b != c and b != d and c != d):
+        break
+print(y)
